40c37a2f39cd59ae94caaf372e4f613a66ba0e8c
HallrizonX <hallrizon@gmail.com>
2020-07-04 16:39:52 +0300

    Init 0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='threador',  # How you named your package folder (MyLib)
     packages=['threador'],  # Chose the same as "name"
-    version='0.1.1',  # Start with a small number and increase it with every change you make
+    version='0.1.2',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='TYPE YOUR DESCRIPTION HERE',  # Give a short description about your library
     author='Kostya Minkov',  # Type in your name
